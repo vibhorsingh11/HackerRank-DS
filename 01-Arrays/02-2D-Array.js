82272d3984b2df02421fbbd573cd15cc40a7fb02
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-12-20 23:21:45 +0530

    Added Challenge 02 to Arrays

---
+++ 01-Arrays/02-2D-Array.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the hourglassSum function below.
+function hourglassSum(arr) {
+    let max = Number.MIN_VALUE;
+    for(let i=0;i<4;i++){
+        for(let j =0;j<4;j++){
+              let sum = (arr[i][j] + arr[i][j + 1] +
+                           arr[i][j + 2]) + (arr[i + 1][j + 1]) +
+                          (arr[i + 2][j] + arr[i + 2][j + 1] +
+                           arr[i + 2][j + 2]);
+        
+        max = Math.max(max, sum);
+        }        
+    }
+    return max;
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    let result = hourglassSum(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
